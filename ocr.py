c2435518b943f61f438d57bce31cf9dc4f72a120
AntoineCompagnie <antoine.compagnie@gmail.com>
2018-11-30 19:17:47 +0100

    created prijatelj branch with its method added to ocr.py

--- ocr.py
+++ ocr.py
@@ -18,8 +18,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-
-
+# For Tesseract to work on windows
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
 # functions
 def pdfspliterimager(filename):
@@ -34,6 +34,42 @@ def pdfspliterimager(filename):
 			page.save('out%s.jpg'%i, 'JPEG')
 		
 		os.remove("document-page%s.pdf" % i)
+		
+
+# removes pixels in image that are between the range of
+# [lower_val,upper_val]
+def remove_gray(img,lower_val,upper_val):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_bound = np.array([0,0,lower_val])
+    upper_bound = np.array([255,255,upper_val])
+    mask = cv2.inRange(gray, lower_bound, upper_bound)
+    return cv2.bitwise_and(gray, gray, mask = mask)
+	
+# finds contours of eroded image
+def prep( img, kern_size = 3 ):    
+    img = erode( img, kern_size )
+    retval, img = cv2.threshold(img, 200.0, 255.0, cv2.THRESH_BINARY_INV) #   invert colors for findContours
+    return cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) # Find Contours of Image
+	
+# given img & number of desired blobs, returns contours of blobs.
+def blobbify(img, num_of_labels, kern_size = 3, dilation_rate = 10):
+    prep_img, contours, hierarchy = prep( img.copy(), kern_size ) # dilate img and check current contour count.
+    while len(contours) > num_of_labels:
+        kern_size += dilation_rate # add dilation_rate to kern_size to increase the blob. Remember kern_size must always be odd.
+        previous = (prep_img, contours, hierarchy)
+        processed_img, contours, hierarchy = prep( img.copy(), kern_size ) # dilate img and check current contour count, again.
+    if len(contours) < num_of_labels:
+        return (processed_img, contours, hierarchy)
+    else:
+        return previous
+
+# finds bounding boxes of all contours
+def bounding_box(contours):
+    bBox = []
+    for curve in contours:
+        box = cv2.boundingRect(curve)
+    bBox.append(box)
+    return bBox
 		
 # variables 
 start = -1
@@ -105,9 +141,11 @@ for file_name in file_names:
 	filename = "{}.png".format(os.getpid())
 	cv2.imwrite(filename, gray)
 	
+	'''
 	# Here we should split the images in parts. Those who have strokes
 	# We asked for a stroke example so we have its color 
 	# While we find pixels with the same color we store its line
+	selecting_area = False
 	im = Image.open(filename)
 	(width, height)= im.size
 	for x in range(width): 
@@ -135,6 +173,7 @@ for file_name in file_names:
 				area = im.crop(text_box)
 				area.show()	
 				break
+	'''
 		    
 	
 	
